d06156a29607f3bb2045f75e0a5893c3d94a99f9
Juliane Holzt <juliane@holzt.de>
2024-12-03 21:53:35 +0100

    refactor: day03

--- day03/day03.go
+++ day03/day03.go
@@ -2,49 +2,48 @@ package main
 
 import (
 	"regexp"
-	"strings"
 
 	"github.com/julijane/advent-of-code-2024/aoc"
 )
 
-func calcFor(line string) int {
+func calcFor(code string, isPart2 bool) int {
 	sum := 0
 
-	mulInstructs := aoc.ExtractRegexps(line, `mul\(\d+\,\d+\)`)
-	for _, mulInstruct := range mulInstructs {
-		numbers := aoc.ExtractNumbers(mulInstruct)
-		sum += numbers[0] * numbers[1]
+	reText := `mul\((\d+),(\d+)\)`
+	if isPart2 {
+		reText += `|don't\(\)|do\(\)`
+	}
+
+	re := regexp.MustCompile(reText)
+
+	mulEnabled := true
+	matches := re.FindAllStringSubmatch(code, -1)
+
+	for _, match := range matches {
+		if match[0] == "don't()" {
+			mulEnabled = false
+		} else if match[0] == "do()" {
+			mulEnabled = true
+		} else if mulEnabled {
+			sum += aoc.Atoi(match[1]) * aoc.Atoi(match[2])
+		}
 	}
 
 	return sum
 }
 
 func calc(input *aoc.Input, doPart1, doPart2 bool) (int, int) {
-	singleLine := ""
-
 	sumPart1 := 0
 	sumPart2 := 0
 
-	for _, line := range input.Lines {
-		singleLine += line.Data
-	}
+	code := input.SingleString()
 
 	if doPart1 {
-		sumPart1 = calcFor(singleLine)
+		sumPart1 = calcFor(code, false)
 	}
 
 	if doPart2 {
-		lastDont := strings.LastIndex(singleLine, "don't()")
-		lastDo := strings.LastIndex(singleLine, "do()")
-
-		if lastDont > lastDo {
-			singleLine = singleLine[:lastDont]
-		}
-
-		re := regexp.MustCompile(`don't\(\).*?do\(\)`)
-		strippedLine := re.ReplaceAllString(singleLine, "")
-
-		sumPart2 = calcFor(strippedLine)
+		sumPart2 = calcFor(code, true)
 	}
 
 	return sumPart1, sumPart2
