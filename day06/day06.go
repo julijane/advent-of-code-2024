6850bc6a425970e71f56ef0a9482db3f46d5c57d
Juliane Holzt <juliane@holzt.de>
2024-12-07 01:09:09 +0100

    refactor: day06

--- day06/day06.go
+++ day06/day06.go
@@ -4,55 +4,66 @@ import (
 	"github.com/julijane/advent-of-code-2024/aoc"
 )
 
-// doWalk walks the grid, returns if it ended up in a loop.
-// If not, it also returns the visited positions (without the startpos)
-// An additional obstacle position can be passed to the function
-func doWalk(grid *aoc.Grid, additionalObstacle aoc.Coordinate, wantVisited bool) (bool, []aoc.Coordinate) {
-	startPos := grid.Find('^')
+func doWalkStep(grid *aoc.Grid, pointer *aoc.Pointer) bool {
+	for {
+		nextContent := grid.Get(pointer.PeekMove(), '~')
 
-	curPointer := aoc.NewPointer(startPos.X, startPos.Y, 0)
-	currentContent := byte('^')
-	visitedWithDirection := make(map[aoc.Pointer]struct{})
-
-	for currentContent != '~' {
-		if _, ok := visitedWithDirection[curPointer]; ok {
-			return true, nil
+		if nextContent == '~' {
+			return false
 		}
 
-		visitedWithDirection[curPointer] = struct{}{}
-
-		nextPos := curPointer.PeekMove()
-		nextContent := grid.Get(nextPos, '~')
-		if nextContent == '#' || nextPos == additionalObstacle {
-			curPointer.TurnRight()
-		} else {
-			curPointer.Move()
-			currentContent = nextContent
+		if nextContent != '#' {
+			pointer.Move()
+			return true
 		}
-	}
 
-	// For part2 we don't need the visited list, so we allow to signal to skip creating it
-	if !wantVisited {
-		return false, nil
+		pointer.TurnRight()
 	}
+}
 
-	// First create a map of visited positions without direction
+func doWalkPart1(grid *aoc.Grid, pos aoc.Coordinate) []aoc.Coordinate {
 	visited := make(map[aoc.Coordinate]struct{})
-	for pointer := range visitedWithDirection {
-		visited[pointer.C] = struct{}{}
+	walker := aoc.Pointer{C: pos, Dir: 0}
+
+	for {
+		visited[walker.C] = struct{}{}
+		if !doWalkStep(grid, &walker) {
+			break
+		}
 	}
 
 	// Then convert the map to a slice
-	visitedSlice := make([]aoc.Coordinate, 0, len(visited)-1)
+	visitedSlice := make([]aoc.Coordinate, len(visited))
+
+	i := 0
 	for coordinate := range visited {
-		// Don't include the startpos, as for part2 we want to skip that
-		// For part1 we can just add one in the caller function
-		if coordinate != startPos {
-			visitedSlice = append(visitedSlice, coordinate)
-		}
+		visitedSlice[i] = coordinate
+		i++
 	}
 
-	return false, visitedSlice
+	return visitedSlice
+}
+
+func doWalkPart2(grid *aoc.Grid, additionalObstacle, startPos aoc.Coordinate) bool {
+	oldContent := grid.Get(additionalObstacle, '~')
+	grid.Set(additionalObstacle, '#')
+	defer grid.Set(additionalObstacle, oldContent)
+
+	slowWalker := aoc.Pointer{C: startPos, Dir: 0}
+	fastWalker := aoc.Pointer{C: startPos, Dir: 0}
+
+	for {
+		doWalkStep(grid, &slowWalker)
+
+		doWalkStep(grid, &fastWalker)
+		if !doWalkStep(grid, &fastWalker) {
+			return false
+		}
+
+		if slowWalker == fastWalker {
+			return true
+		}
+	}
 }
 
 func calc(input *aoc.Input, doPart1, doPart2 bool) (int, int) {
@@ -60,16 +71,17 @@ func calc(input *aoc.Input, doPart1, doPart2 bool) (int, int) {
 
 	grid := input.Grid()
 
+	startPos := grid.Find('^')
+
 	// Part1
-	// We just need to walk until we exit the grid and count the visited positions
-	_, visited := doWalk(grid, aoc.Coordinate{X: -2, Y: 0}, true)
-	sumPart1 := len(visited) + 1
+	visited := doWalkPart1(grid, startPos)
+	sumPart1 := len(visited)
 
 	// Part2
 	// We can only set one obstacle at a time, so we only need to test the positions
 	// visited in Part1 as potential obstacle positions
 	for _, additionalObstaclePos := range visited {
-		if loop, _ := doWalk(grid, additionalObstaclePos, false); loop {
+		if doWalkPart2(grid, additionalObstaclePos, startPos) {
 			sumPart2++
 		}
 	}
